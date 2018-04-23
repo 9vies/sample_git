a47aa9061807950abdc639da16108378721efe64
9vies <dragonslayer834@gmail.com>
2018-04-23 17:21:54 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv)
+{
+	string string_to_print;
+	cin >> string_to_print; 
+	cout << string_to_print << '\n';
+
+	return 0;
+}
